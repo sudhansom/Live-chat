4a2283a3376369a5a5f9a18669496396cbb10a1f
bhuwan <bkspoudel@gmail.com>
2022-02-25 15:49:16 +0100

    input values controlled

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
 import "./App.css";
 import io from "socket.io-client";
+import { useState } from "react";
 
 // eslint-disable-next-line
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [user, setUser] = useState("");
+  const [room, setRoom] = useState("");
+  const joinRoom = () => {};
+
   return (
     <div className="App">
       <h1>Join A Chat</h1>
-      <input type="text" placeholder="hello..."></input>
-      <input type="text" placeholder="room"></input>
+      <input
+        type="text"
+        placeholder="hello..."
+        onChange={(event) => {
+          setUser(event.target.value);
+        }}
+        value={user}
+      />
+      <input
+        type="text"
+        placeholder="room"
+        onChange={(event) => {
+          setUser(event.target.value);
+        }}
+        value={room}
+      />
       <button>Join Room</button>
     </div>
   );
