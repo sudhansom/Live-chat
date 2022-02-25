aecca26ef781d54a75ddaf9f5de5b87aedb4db29
bhuwan <bkspoudel@gmail.com>
2022-02-25 14:57:17 +0100

    added server/index.js and ran the server at 3001

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
-import express from "express";
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+// import express from "express";
+// import http from "http";
+// import cors from "cors";
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("server is listening to the port 5000");
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING...");
 });
