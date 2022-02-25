ba91b59833f33d881209f8086d1882319dfac113
bhuwan <bkspoudel@gmail.com>
2022-02-25 15:13:12 +0100

    added .gitignore to ignore node_modules

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
+
 // import express from "express";
 // import http from "http";
 // import cors from "cors";
@@ -10,6 +12,13 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, () => {
+  cors: {
+    origin: "http://localhost:3000";
+    methods: ["GET", "POST"];
+  }
+});
 
 server.listen(3001, () => {
   console.log("SERVER RUNNING...");
